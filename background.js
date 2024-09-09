46c9216a95fb1333076527c730a3c741e62add91
098orin <mario3.14_luigi1.618@docomo.ne.jp>
2024-09-09 18:27:27 +0900

    fix: typescript

--- background.js
+++ background.js
@@ -2,27 +2,7 @@ const DBname = "IMG_data_saver"
 const DBvar = 3
 const DBOpenRequest = window.indexedDB.open(DBname, DBvar);
 function get_redirect_url(requestURL) {
-  // 処理が成功しなかった場合を考慮してエラーをキャッチできるようにtryを利用します
-  try {
-    request.onupgradeneeded = (event) => {
-      // event.targetをキャストしないと型エラーで怒られます
-      const db = (event.target as IDBRequest).result as IDBDatabase;
-      if (!db.objectStoreNames.contains(requestURL)) {
-        return;
-      }
-    };
-  
-    request.onsuccess = (event) => {
-      const db = (event.target as IDBRequest).result as IDBDatabase;
-      resolve(db);
-    };
-  
-    request.onerror = (event) => {
-      reject((event.target as IDBRequest).error);
-    };
-  } catch (error) {
-    reject(error);
-  }
+
 }
 
 
