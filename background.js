d35602d23907240129458810638b66e25bf44cdc
098orin <mario3.14_luigi1.618@docomo.ne.jp>
2024-09-09 20:18:24 +0900

    半分くらいできた <= v.0.0.5

--- background.js
+++ background.js
@@ -1,9 +1,26 @@
 const DBname = "IMG_data_saver" 
-const DBvar = 3
-const DBOpenRequest = window.indexedDB.open(DBname, DBvar);
-function get_redirect_url(requestURL) {
+const DBvar = 1
+const DBRequest = window.indexedDB.open(DBname, DBvar);
 
+DBrequest.onerror = (event) => {
+  console.error(`Database error: ${event.target.errorCode}`);
+};
+
+
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  db.createObjectStore("IMG", {keyPath : 'id'})
+};
+
+
+function get_redirect_url(requestURL) {
+  
 }
+
+function add_archive(requestURL){
+  const data = requestURL
+}
+
 
 
 function redirect(requestDetails) {
@@ -17,7 +34,35 @@ function redirect(requestDetails) {
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
-  redirect,
+  (details) => {  redirect(details),
   { urls: [pattern], types: ["image"] },
-  ["blocking"],
+  ["blocking"] }
 );
+/*
+chrome.webRequest.onCompleted.addListener(
+  (details) => { add_archive(details.url) }
+);
+*/
+
+function getFileExtension(contentType) {
+  if (contentType.includes("application/json")) {
+    return ".json";
+  } else if (contentType.includes("text/html")) {
+    return ".html";
+  } else if (contentType.includes("text/plain")) {
+    return ".txt";
+  } else if (contentType.includes("application/javascript")) {
+    return ".js";
+  } else if (contentType.includes("image/png")) {
+    return ".png";
+  } else if (contentType.includes("image/jpeg")) {
+    return ".jpg";
+  } else if (contentType.includes("image/gif")) {
+    return ".gif";
+  } else if (contentType.includes("application/xml")) {
+    return ".xml";
+  } else if (contentType.includes("application/octet-stream")) {
+    return ".bin"; // バイナリデータの場合
+  }
+  return ""; // 拡張子がわからない場合は空文字
+}
